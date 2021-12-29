6ebc64b4fb57d21d7e010657c953b5c77b7b7360
Xiguac <58496501+xiguac@users.noreply.github.com>
2021-12-29 20:51:40 +0800

    信息配置文件

---
+++ ks2shop_config.php
@@ -0,0 +1,21 @@
+<?php
+//数据库配置
+$servername = "localhost";//数据库连接地址
+$username = "ks2shop";//数据库用户名
+$password = "ks2shop";//数据库密码
+
+//易支付Url,以http://或https://开头，以/结尾
+$ks2_shop_url = "";
+//易支付id
+$ks2shop['id'] = "";
+//易支付key
+$ks2shop['key'] = "";
+
+//服务器uid
+$ks2_id = "";
+//服务器key
+$ks2_key = "";
+//需要执行的指令
+//玩家名变量{playername},金额变量{amount}
+$ks2_command = "say {playername} {amount}";
+?>
